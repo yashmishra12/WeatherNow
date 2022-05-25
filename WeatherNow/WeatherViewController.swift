d65bf49655b17d48dc271cc0d266d27571ad3a73
Yash Mishra <yashmishra12@hotmail.com>
2022-05-24 21:01:31 -0400

    layout constraints set

---
+++ WeatherNow/WeatherViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeatherNow
+//
+//  Created by Yash Mishra on 5/24/22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
